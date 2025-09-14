51f29853aed13648b1d586d3ac63227377f55791
Bhall2023 <147952158+Bhall2023@users.noreply.github.com>
2025-09-14 10:40:56 -0500

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 
 export default function App() {
   return <div className="App">
+    <div className="container">
     <h1>Weather App</h1>
   <footer>
   This project was coded by <a href = "https://www.shecodes.io/graduates/162107-bonnie-hall" target="_blank" rel="noopener noreferrer">Bonnie Hall</a> and is open-sourced on {" "}
   <a href = "https://github.com/Bhall2023/react-project" target="_blank" rel="noopener noreferrer">GitHub</a>
   </footer>
-  </div>;
+  </div>
+  </div>
 }
 
