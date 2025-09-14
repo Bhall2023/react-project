01ed66c7c15b964c048691ac6fce0ed8a0c9809c
Bhall2023 <147952158+Bhall2023@users.noreply.github.com>
2025-09-14 11:21:33 -0500

    added html, some css styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import Weather from "./Weather";  
+import "./App.css";
 
 
 export default function App() {
   return <div className="App">
     <div className="container">
+      <Weather />
     <h1>Weather App</h1>
+    
   <footer>
   This project was coded by <a href = "https://www.shecodes.io/graduates/162107-bonnie-hall" target="_blank" rel="noopener noreferrer">Bonnie Hall</a> and is open-sourced on {" "}
   <a href = "https://github.com/Bhall2023/react-project" target="_blank" rel="noopener noreferrer">GitHub</a>
